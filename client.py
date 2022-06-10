7267ab8892c1c9fd65f44ec3e06d3b779f9aa297
Iqbal Dzakwan I <41475575+dzakw@users.noreply.github.com>
2022-06-10 13:28:18 +0700

    finalisasi socket programming

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import elgamal
 import pickle
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-client.connect(('localhost', 1002))  # 127.0.0.1
+client.connect(('192.168.150.231', 1024))  # 127.0.0.1
 
 msg = input("msg: ")
 q=elgamal.q
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import elgamal
 import pickle
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # AF_INET = IP, SOCK_STREAM = TCP
-server.bind(('localhost', 1002))  # 127.0.0.1
+server.bind(('0.0.0.0', 1002))  # 127.0.0.1
 server.listen()
 
 client_socket, client_address = server.accept()
